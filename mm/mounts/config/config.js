0ff868647324ef98caca7c9577a8bf0a24af62cf
matthes-me <52692176+matthes-me@users.noreply.github.com>
2023-07-25 21:26:17 +0200

    docker
    pulll

---
+++ mm/mounts/config/config.js
@@ -0,0 +1,64 @@
+/* MagicMirror² Config Sample
+ *
+ * By Michael Teeuw https://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information on how you can configure this file
+ * see https://docs.magicmirror.builders/configuration/introduction.html
+ * and https://docs.magicmirror.builders/modules/configuration.html
+ *
+ * You can use environment variables using a `config.js.template` file instead of `config.js`
+ * which will be converted to `config.js` while starting. For more information
+ * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
+ */
+let config = {
+	address: "0.0.0.0",	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/",			// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+					  		// you must set the sub path here. basePath must end with a /
+	ipWhitelist: [],	// Set [] to allow all IP addresses
+															// or add a specific IPv4 of 192.168.1.5 :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "de",
+	locale: "de-DE",
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric",
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_left",
+
+			config: {
+				feeds: [
+					{
+						title: "Mastodon",
+						url: "http://10.45.0.108:3000/?action=display&bridge=FeedMergeBridge&feed_name=KantineFestival&feed_1=https%3A%2F%2Fmastodon.social%2Ftags%2Fkantinefestival.rss&feed_2=https%3A%2F%2Fmastodon.social%2Ftags%2Fsubbotnik.rss&feed_3=https%3A%2F%2Fmastodon.social%2Ftags%2Fkantinesabot.rss&feed_4=https%3A%2F%2Fmastodon.social%2Ftags%2Fsubbotnikchemnitz.rss&feed_5=https%3A%2F%2Fmastodon.social%2Ftags%2Fchemnitz.rss&feed_6=&feed_7=&feed_8=&feed_9=&feed_10=&limit=&format=Atom"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = config;}
